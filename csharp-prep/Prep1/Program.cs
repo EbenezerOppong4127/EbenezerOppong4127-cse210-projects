d42c3354d2b35a936e572045e048eed384def785
EbenezerOppong4127 <95159003+EbenezerOppong4127@users.noreply.github.com>
2024-01-11 09:25:06 +0000

    push2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Let print a message
         Console.Write("What is your first name ?");
+        
+        //Let get  input from the user
         string first = Console.ReadLine();
         
+        // Let print a message
         Console.Write("What is your last name ?");
+       
+        //Let get  input from the user
         string last = Console.ReadLine();
         
+        // Let print a message
         Console.WriteLine($"Your name is {last} , {first}  {last}");
     }
 }
